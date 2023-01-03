237ef555f1df9cf930e52f6b238f9052adaa95dc
Salih <salihcakmak700@gmail.com>
2023-01-03 03:05:29 +0300

    initial commits

---
+++ constants/percantages.py
@@ -0,0 +1,2 @@
+values = {"belirsiz": -1, "sifir": 0, "on": 0.1, "yirmi": 0.2, "otuz": 0.3, "kirk": 0.4, "elli": 0.5, "altmis": 0.6,
+          "yetmis": 0.7, "seksen": 0.8, "doksan": 0.9, "yuz": 1, "bos": 0, "yari": 0.5, "dolu": 1}
