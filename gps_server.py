3a3f333580203e13b82e12599bb4545fc5f351ba
Salih <salihcakmak700@gmail.com>
2023-01-13 15:46:24 +0300

    initial commits

--- gps_server.py
+++ gps_server.py
@@ -5,6 +5,14 @@ from threading import Thread
 from constants.gps_commands import *
 
 
+# convert decimal degrees to degrees, minutes and seconds
+def dd2dms(dd):
+    dd = float(dd)
+    mnt, sec = divmod(dd * 3600, 60)
+    deg, mnt = divmod(mnt, 60)
+    return round(deg * 100 + mnt + sec / 100, 4)
+
+
 class Server(SerialConnection, Thread):
     def __init__(self, port, baudrate, timeout):
         Thread.__init__(self, daemon=True, name="Server")
@@ -20,9 +28,11 @@ class Server(SerialConnection, Thread):
             if data.startswith(POWER_UP):
                 self.send_command(OK)
             elif data.startswith(GET_GPS_DATA):
+                gps_location = "41.14231312833451,28.79271038554962".split(",")
+                lat, lng = map(dd2dms, gps_location)
                 local_time = datetime.now().strftime("%H%M%S.%f")
                 local_date = datetime.now().strftime("%d%m%y")
-                fix_data = MANUEL_FIX_DATA.format(local_time, local_date)
+                fix_data = MANUEL_FIX_DATA.format(local_time, lat, lng, local_date)
                 self.send_command(GPS_DATA + fix_data)
                 self.send_command(LINE)
                 self.send_command(OK)
