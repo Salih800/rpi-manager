70acb7c4bafe5491f4cd4eccf889fecf9a96546b
Salih <salihcakmak700@gmail.com>
2023-01-13 14:51:33 +0300

    initial commits

--- tools.py
+++ tools.py
@@ -146,7 +146,7 @@ def check_system_time(gps_local_time):
 
 # install repo requirements if not installed
 def install_requirements():
-    if subprocess.call(["pip", "install", "-r", "requirements.txt"]) == 0:
+    if subprocess.call(["pip3", "install", "-r", "requirements.txt"]) == 0:
         logging.info("Requirements installed.")
         return True
     else:
