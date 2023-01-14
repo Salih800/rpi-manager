1466853cdcad696f1472023862396508355e9cc5
Salih <salihcakmak700@gmail.com>
2023-01-14 04:30:59 +0300

    initial commits

--- src/camera_manager.py
+++ src/camera_manager.py
@@ -53,7 +53,8 @@ class CameraManager(Thread, metaclass=Singleton):
             ret, frame = self.camera.read()
             if ret:
                 frame = imutils.rotate(frame, self.rotation)
-                draw_text(frame, str(datetime.now()), (20, 60), text_size=1)
+                draw_text(frame, datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                          (20, 60), text_size=1, text_thickness=2)
                 self.last_frame = frame
             else:
                 self.last_frame = None
