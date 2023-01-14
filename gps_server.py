e095d1bd32d8b5e4b8ca2a366055b236e66a923e
Salih <salihcakmak700@gmail.com>
2023-01-14 03:36:24 +0300

    initial commits

--- gps_server.py
+++ gps_server.py
@@ -28,7 +28,7 @@ class Server(SerialConnection, Thread):
             if data.startswith(POWER_UP):
                 self.send_command(OK)
             elif data.startswith(GET_GPS_DATA):
-                gps_location = "40.78843473159141, 29.44004852675274".split(",")
+                gps_location = "41.082763305556,28.785443305556".split(",")
                 lat, lng = map(dd2dms, gps_location)
                 local_time = datetime.now().strftime("%H%M%S.%f")
                 local_date = datetime.now().strftime("%d%m%y")
