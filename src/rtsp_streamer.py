79149f1f432cfa4066a66f09be2bc171abf9c4a7
Salih <salihcakmak700@gmail.com>
2023-01-20 01:03:24 +0300

    initial commits

--- src/rtsp_streamer.py
+++ src/rtsp_streamer.py
@@ -1,4 +1,6 @@
 import logging
+import os.path
+import time
 from threading import Thread
 import subprocess
 # from queue import Queue
@@ -6,7 +8,10 @@ import subprocess
 # import time
 import numpy as np
 import psutil
+
+from constants.folders import camera_records
 from constants.urls import url_stream
+from utils import request_handler as rh
 
 
 def check_process(pid):
@@ -22,18 +27,27 @@ def check_process(pid):
     return False
 
 
-def writer(path, width, height, fps=25, loglevel='warning'):
+def writer(path, width=640, height=480, fps=25, loglevel='warning'):
+    if not os.path.isdir(camera_records):
+        os.mkdir(camera_records)
     logging.info(f"Starting writer: {path} @ {width}x{height} @ {fps}fps")
-    command = ["ffmpeg", "-f", "rawvideo", "-pix_fmt", "rgb24",
-               "-s", f"{width}x{height}",
-               "-r", f"{fps}",
-               "-i", "pipe:",
-               "-vf", f"fps={fps}",
-               "-c:v", "libx264", "-preset", "ultrafast", "-tune", "zerolatency",
-               # "-vf", ("drawtext=x=10:y=10:fontsize=24:fontcolor=white:"
-               #         "text='%{localtime\:%Y-%m-%d %H.%M.%S}':box=1:boxcolor=black@1"),
-               "-pix_fmt", "yuv420p", "-loglevel", loglevel,
-               "-f", "rtsp", "-rtsp_transport", "tcp", path]
+    # command = ["ffmpeg", "-f", "rawvideo", "-pix_fmt", "rgb24",
+    #            "-s", f"{width}x{height}",
+    #            "-r", f"{fps}",
+    #            "-i", "pipe:",
+    #            "-vf", f"fps={fps}",
+    #            "-c:v", "libx264", "-preset", "ultrafast", "-tune", "zerolatency",
+    #            # "-vf", ("drawtext=x=10:y=10:fontsize=24:fontcolor=white:"
+    #            #         "text='%{localtime\:%Y-%m-%d %H.%M.%S}':box=1:boxcolor=black@1"),
+    #            "-pix_fmt", "yuv420p", "-loglevel", loglevel,
+    #            "-f", "rtsp", "-rtsp_transport", "tcp", path]
+    command = (f"ffmpeg -i " + path + " " +
+               f"-c:v libx264 -preset ultrafast -tune zerolatency -crf 25 -loglevel {loglevel} "
+               f"-vf ""drawtext=x=10:y=10:fontsize=24:fontcolor=white:text='%{localtime}':box=1:boxcolor=black@1"" "
+               "-f tee -map 0:v """"[f=segment:segment_list=out.list:segment_time=600:segment_wrap=6:strftime=1]'"""""
+               + camera_records +
+               """%Y-%m-%d_%H-%M-%S.mp4'|[f=rtsp:rtsp_transport=tcp]""" + url_stream).split()
+    logging.info(f"Starting writer: {command}")
     return subprocess.Popen(command, stdin=subprocess.PIPE)
 
 
@@ -75,14 +89,16 @@ class RTSPStreamer(Thread):
 
     def run(self):
         while self.running:
+            self.start_writer()
+            time.sleep(5)
             # frame = self.writer_queue.get()
             # start_time = time.time()
-            frame = self._parent.camera_manager.get_frame()
-            # get_time = time.time() - start_time
-            if frame is None:
-                self.stop()
-                break
-            self.write(frame)
+            # frame = self._parent.camera_manager.get_frame()
+            # # get_time = time.time() - start_time
+            # if frame is None:
+            #     self.stop()
+            #     break
+            # self.write(frame)
             # write_time = time.time() - start_time - get_time
             # logging.info(f"Get frame: {get_time:.4f}s, "
             #              f"Write frame: {write_time:.4f}s, "
@@ -91,20 +107,16 @@ class RTSPStreamer(Thread):
     def start_writer(self):
         # start_time = time.time()
         if self.writer is not None:
-            # if not check_process(self.writer.pid):
-            #     return
-            return
+            if check_process(self.writer.pid):
+                return
         else:
-            settings = self._parent.camera_manager.get_camera_info()
-            self.writer = writer(self.url,
-                                 settings["width"],
-                                 settings["height"],
-                                 settings["fps"])
+            # settings = self._parent.camera_manager.get_camera_info()
+            self.writer = writer(self.url)
         # logging.info(f"Writer started in {time.time() - start_time:.4f}s")
 
     def stop_writer(self):
         if self.writer is not None:
-            self.writer.stdin.close()
+            self.writer.kill()
             self.writer.wait()
             self.writer = None
 
