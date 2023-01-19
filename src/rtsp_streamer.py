9dd3dfab470df6e8416076b7e132f4264d653ca6
Salih <salihcakmak700@gmail.com>
2023-01-20 01:24:04 +0300

    initial commits

--- src/rtsp_streamer.py
+++ src/rtsp_streamer.py
@@ -41,7 +41,7 @@ def writer(path, width=640, height=480, fps=25, loglevel='warning'):
     #            #         "text='%{localtime\:%Y-%m-%d %H.%M.%S}':box=1:boxcolor=black@1"),
     #            "-pix_fmt", "yuv420p", "-loglevel", loglevel,
     #            "-f", "rtsp", "-rtsp_transport", "tcp", path]
-    command = (f"ffmpeg -s" + f"{width}x{height}" + "-i /dev/video0 " +
+    command = (f"ffmpeg -s " + f"{width}x{height} " + "-i /dev/video0 " +
                f"-c:v libx264 -preset ultrafast -tune zerolatency -crf 25 -loglevel {loglevel} "
                f"-vf ""drawtext=x=10:y=10:fontsize=24:fontcolor=white:text='%{localtime}':box=1:boxcolor=black@1"" "
                "-f tee -map 0:v """"[f=segment:segment_list=out.list:segment_time=600:segment_wrap=6:strftime=1]'"""""
