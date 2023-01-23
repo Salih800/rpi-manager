2da64929c736e7d9d9ba521da070624c230670f4
Salih <salihcakmak700@gmail.com>
2023-01-24 01:13:14 +0300

    SpmManager added rst_spm

--- src/system_checker.py
+++ src/system_checker.py
@@ -4,6 +4,7 @@ import threading
 import shutil
 import time
 import psutil
+from datetime import timedelta
 
 from utils.size_converter import SizeConverter
 
@@ -19,7 +20,7 @@ class SystemChecker(threading.Thread):
         self.used_memory_percent = 0
 
         self.cpu_usage = 0
-
+        self.system_start_time = time.time()
         self.proces_memory = 0
 
         self.total_disk = 0
@@ -30,10 +31,11 @@ class SystemChecker(threading.Thread):
 
     def __str__(self):
         return (f"System Info: "
-                f"Process Memory: {self.proces_memory}, "
-                f"Memory Usage: {self.used_memory_percent}%, "
-                f"CPU: {self.cpu_usage}%, "
-                f"Disk Usage: {self.disk_usage_percent}%")
+                f"Process Memory: {self.proces_memory} | "
+                f"Memory Usage: {self.used_memory_percent}% | "
+                f"CPU: {self.cpu_usage}% | "
+                f"Disk Usage: {self.disk_usage_percent}% | "
+                f"System Uptime: {self.get_system_uptime()}")
 
     def is_enough_memory(self, percent=90) -> bool:
         return self.used_memory_percent <= percent
@@ -72,3 +74,6 @@ class SystemChecker(threading.Thread):
             self.get_disk_usage()
             self.get_cpu_usage()
             time.sleep(1)
+
+    def get_system_uptime(self):
+        return timedelta(seconds=(time.time() - self.system_start_time))
